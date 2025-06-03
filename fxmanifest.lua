eb47d504279a5303bb2bebd3af33cb785b8ce61e
Zevarda <khoironisubagio01@gmail.com>
2025-06-03 08:43:14 +0700

    upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'zDev'
+description 'Multi-Framework Vehicle Rental Script'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependencies {
+    'ox_lib', 
+}
